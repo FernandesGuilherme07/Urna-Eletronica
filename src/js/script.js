82409399463c7396016ad968bccf04c09c821d76
Guilherme Fernandes <guifhunter28@gmail.com>
2021-03-31 13:23:56 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,26 @@ let Warning = document.querySelector('.d-2');
 let side = document.querySelector('.d-1-right');
 let numbers = document.querySelector('d-1-3');
 
+let currentStep = 0;
+let number = '';
+
+function startStage() {
+    let stage = stage[startStage];
+
+    let numberHtml = '';
+
+    for (let i=0; i<stage.numbers;i++) {
+        numberHtml += '<div class="number"></div>';
+    }
+
+    youVoteFor.style.display = 'none';
+    office.innerHTML = stage.title;
+    descrition.innerHTML = '';
+    Warning.display = 'none';
+    side.innerHTML = '';
+    numbers.innerHTML = numberHtml;
+}
+
 
 function clicou(n) {
     alert(`clicou em ${n}`)
@@ -21,3 +41,5 @@ function clicou(corrects) {
 function clicou(CONFIRMS) {
     alert(`clicou em ${CONFIRMS}`)
 }
+
+startStage();

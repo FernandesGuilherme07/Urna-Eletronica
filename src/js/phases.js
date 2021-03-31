21c36e74a10ba2060fda8a227b4e547150e4f714
Guilherme Fernandes <guifhunter28@gmail.com>
2021-03-31 11:44:12 -0300

    Create phases.js

---
+++ src/js/phases.js
@@ -0,0 +1,49 @@
+let estapas = [
+    {
+        title: 'VEREADOR',
+        numbers: 5,
+        candidates: [
+            {
+                number:12345,
+                name: 'Fulano de Tal',
+                broken: 'ABC',
+                photos:[
+                    {url:'2021-03-30 (4).png', leg: 'Vereador'}
+                ]
+            },
+            {
+                number:56789,
+                name: 'Beltrano de Tal',
+                broken: 'CBB',
+                photos:[
+                    {url:'2021-03-30 (5).png', leg: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        title: 'PREFEITO',
+        numbers: 5,
+        candidates: [
+            {
+                number:12,
+                name: 'Arquimendes',
+                broken: 'ABC',
+                vice: Marilda,
+                photos:[
+                    {url:'2021-03-30 (2).png', leg: 'Prefeito'},
+                    {url:'2021-03-30 (3).png', leg: 'Vice', small: true}
+                ]
+            },
+            {
+                number:56,
+                name: 'Santana',
+                broken: 'CBB',
+                vice: Ciço,
+                photos:[
+                    {url:'2021-03-30 (6).png', leg: 'Prefeito'},
+                    {url:'2021-03-30 (7).png', leg: 'Vice', small: true}
+            }
+        ]
+    },  
+]

c2c15651ea22ae0241f51fd281698dbf6bdb1709
Guilherme Fernandes <guifhunter28@gmail.com>
2021-03-31 12:26:13 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -4,3 +4,20 @@ let descrition = document.querySelector('d-1-4');
 let Warning = document.querySelector('.d-2');
 let side = document.querySelector('.d-1-right');
 let numbers = document.querySelector('d-1-3');
+
+
+function clicou(n) {
+    alert(`clicou em ${n}`)
+}
+
+function clicou(white) {
+    alert(`clicou em ${white}`)
+}
+
+function clicou(corrects) {
+    alert(`clicou em ${corrects}`)
+}
+
+function clicou(CONFIRMS) {
+    alert(`clicou em ${CONFIRMS}`)
+}

101abe820c65f69a84195490bdc4e7285e6c99d5
Guilherme Fernandes <guifhunter28@gmail.com>
2021-03-31 12:20:49 -0300

    Update script.js

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+let youVoteFor = document.querySelector('.d-1-1 span');
+let office =  document.querySelector('.d-1-2 span');
+let descrition = document.querySelector('d-1-4');
+let Warning = document.querySelector('.d-2');
+let side = document.querySelector('.d-1-right');
+let numbers = document.querySelector('d-1-3');
